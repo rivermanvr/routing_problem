94c6cd338500c9926151cf443c3ddb0f4889d2e1
rivermanvr <vrgithub@riversconsulting.com>
2018-08-16 14:11:40 -0400

    quick mods to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Courses from './containers/Courses/Courses';
@@ -23,5 +22,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
