3819bf6d25653531cf69cf02a17fbc5cab9d125e
rivermanvr <vrgithub@riversconsulting.com>
2018-08-16 15:29:15 -0400

    setup Router in index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import './App.css';
 
 import Courses from './containers/Courses/Courses';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'normalize.css/normalize.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element = (
+  <Router>
+    <App />
+  </Router>
+);
+
+ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
